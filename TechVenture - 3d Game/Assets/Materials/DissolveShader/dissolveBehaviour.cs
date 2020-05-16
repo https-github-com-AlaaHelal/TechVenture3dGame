196ab943cf7916665266571cf0c5bd4c960ad445
Sara-Ahmed98 <65094880+Sara-Ahmed98@users.noreply.github.com>
2020-05-16 22:19:40 +0200

    CapsuleRoom components
    Adding the components of the room

---
+++ TechVenture - 3d Game/Assets/Materials/DissolveShader/dissolveBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dissolveBehaviour : MonoBehaviour
+{ 
+    public Material dissolveMat;
+    public float dissolveSpeed = 0.02f;
+    float dissolveAmount = -1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dissolveMat.SetFloat("Vector1_D7B00FB3", dissolveAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dissolveAmount < 1)
+        { 
+            dissolveAmount += dissolveSpeed * Time.deltaTime;
+            dissolveMat.SetFloat("Vector1_D7B00FB3", dissolveAmount);
+            Debug.Log(dissolveAmount);
+        }
+    }
+}
