3b66daa60813c630ede2612abc62036b9dbb7064
amirasayed <49746667+amirasayed@users.noreply.github.com>
2020-05-17 22:25:53 +0200

    Add character and Screen
    -creat animation controller for character movement and pickup items .
    - creat screen for informations and Questions

---
+++ TechVenture - 3d Game/Assets/Scripts/Educational Puzzles/Educational.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Educational : MonoBehaviour
+{
+    public Canvas ScreenCanvas;
+    public Animator[] ballAnimatore = new Animator[20];
+    public Animator baseScreen;
+    public Animator Screen;
+    public GameObject[] Balls = new GameObject[20];
+    public GameObject[] InformationText = new GameObject[10];
+    public  GameObject[] QuestionPanels = new GameObject[10];
+    int Number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //deactive canvas and texts;
+       ScreenCanvas.enabled = false;
+
+        for (int i = 0; i < 10; i++)
+        {
+            InformationText[i].SetActive(false);
+            QuestionPanels[i].SetActive(false);
+        }
+
+
+    }
+  
+    //when touch the ball 
+    void OnTriggerEnter(Collider Ball)
+    {
+        if (Ball.gameObject.tag == "BinaryInformation")
+        {
+            Number = 0;
+            Display();
+
+        }
+        if (Ball.gameObject.tag == "LogicInformation")
+        {
+            Number = 1;
+            Display();
+
+        }
+        if (Ball.gameObject.tag == "BinaryQuestion")
+        {
+            Number = 10;
+            Display();
+
+
+        }
+        if (Ball.gameObject.tag == "LogicQuestion")
+        {
+            Number = 11;
+            Display();
+
+
+        }
+    }
+    //Display screen
+    void Display()
+    {
+        ballAnimatore[Number].SetBool("is interacted", true);
+        ScreenCanvas.enabled = true;
+        StartCoroutine(TimeToShowScreen(Number));
+
+    }
+    IEnumerator TimeToShowScreen(int Number)
+    {
+        yield return new WaitForSeconds(4f);
+        baseScreen.SetBool("open", true);
+        yield return new WaitForSeconds(2f);
+        Screen.SetBool("open", true);
+        yield return new WaitForSeconds(1.5f);
+        DisplayInformationORQuestion(Number);
+        Balls[Number].SetActive(false);
+
+
+
+
+    }
+    //hide screen when Exit
+    IEnumerator TimeToHideScreen(int Number)
+    {
+        yield return new WaitForSeconds(.5f);
+        HideInformationORQuestion(Number);
+        yield return new WaitForSeconds(1f);
+        Screen.SetBool("open", false);
+        yield return new WaitForSeconds(1f);
+        baseScreen.SetBool("open", false);
+        yield return new WaitForSeconds(1.5f);
+        ScreenCanvas.enabled = false;
+
+    }
+    
+   
+    void DisplayInformationORQuestion(int Number)
+    {
+        if (Number < 10)
+        {
+            InformationText[Number].gameObject.SetActive(true);
+        }
+        if (Number >= 10)
+        {
+
+            QuestionPanels[Number - 10].gameObject.SetActive(true);
+        }
+    }
+    void HideInformationORQuestion(int Number)
+    {
+        if (Number < 10)
+        {
+            InformationText[Number].gameObject.SetActive(false);
+        }
+        if (Number >= 10)
+            
+        {
+            QuestionPanels[Number - 10].gameObject.SetActive(false);
+        }
+    }
+   public void Exit()
+    {
+        StartCoroutine(TimeToHideScreen(Number));
+    }
+
+
+}
---
+++ TechVenture - 3d Game/Assets/Scripts/Player/MOVE.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVE : MonoBehaviour
+{
+    public float Maxspeed;
+    public float RotatSpeed;
+    public Animator character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float walkforward = Input.GetAxis("Vertical") * Maxspeed * Time.deltaTime;
+        transform.Translate(0, 0, walkforward);
+        float rotation = Input.GetAxis("Horizontal") * RotatSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+        if (walkforward > 0)
+        {
+            character.SetBool("front", true);
+            character.SetBool("back", false);
+         
+        }
+        else if (walkforward < 0)
+        {
+            character.SetBool("back", true);
+            character.SetBool("front", false);
+
+        }
+        else
+        {
+            character.SetBool("back", false);
+            character.SetBool("front", false);
+        }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Maxspeed = 0;
+            RotatSpeed = 0;
+            character.SetBool("pickup", true);
+            character.SetBool("pickupmid", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            Maxspeed = 5;
+            RotatSpeed = 50;
+
+            character.SetBool("pickupmid", false);
+            character.SetBool("pickup", false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Maxspeed = 0;
+            RotatSpeed = 0;
+            character.SetBool("pickup", true);
+            character.SetBool("pickuplow", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            Maxspeed = 5;
+            RotatSpeed = 50;
+
+            character.SetBool("pickuplow", false);
+            character.SetBool("pickup", false);
+
+        }
+    }
+}
