3d9f6e935fdb2e43adea3e714b2fd27a03f9b19d
AlaaHelal <alaa.helal98@gmail.com>
2020-05-20 02:42:32 +0200

    Capsule Room
    1) Removed "character movement" script.
    2) Changed the name of "MOVE" script into "CharacterAnimatin" script.
    3) Edited CapsuleRoom Position.
    4) Edited the player start position.
    5) Edited the player Tag.
    6) Edited the name of all the objects.
    7) Added Collider for each object in the room.
    8) Collecting items script.
    9) UI Scripts folder.
    10) Camera Controller Scripts Folder.

---
+++ TechVenture - 3d Game/Assets/Scripts/Items/CollectingItems.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectingItems : MonoBehaviour
+{
+    public GameObject Player;
+    bool nearToItem = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == this.transform)
+                {
+                    if (!nearToItem)
+                        print("Get Closer");
+                }
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("press Space");
+            nearToItem = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                this.gameObject.SetActive(false);
+        }
+    }
+
+
+}
+
--- TechVenture - 3d Game/Assets/Scripts/Player/CharacterAnimation.cs
+++ TechVenture - 3d Game/Assets/Scripts/Player/CharacterAnimation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MOVE : MonoBehaviour
+public class CharacterAnimation : MonoBehaviour
 {
     public float Maxspeed;
     public float RotatSpeed;
