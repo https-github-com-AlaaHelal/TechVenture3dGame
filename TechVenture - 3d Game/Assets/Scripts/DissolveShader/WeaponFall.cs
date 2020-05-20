e312ea98b9c514ede2a619838aeed139997766ed
Sara-Ahmed98 <65094880+Sara-Ahmed98@users.noreply.github.com>
2020-05-20 18:19:57 +0200

    Dissolve Ball
    Added dissolveBehaviour scrpit to control the dissolve shader
    
    Added ICMat folder

---
+++ TechVenture - 3d Game/Assets/Scripts/DissolveShader/WeaponFall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFall : MonoBehaviour
+{
+    public GameObject OrbShield;
+
+    private dissolveBehaviour behaviour;
+    private Rigidbody WeaponRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        behaviour = OrbShield.GetComponent<dissolveBehaviour>();
+        WeaponRb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (behaviour.Fall)
+        {
+            Destroy(OrbShield);
+            WeaponRb.useGravity = true;
+        }
+    }
+}
---
+++ TechVenture - 3d Game/Assets/Scripts/DissolveShader/dissolveBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dissolveBehaviour : MonoBehaviour
+{
+    //public Material dissolveMat;
+    //public float dissolveSpeed = 0.02f;
+    //float dissolveAmount = -1;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    dissolveMat.SetFloat("Vector1_D7B00FB3", dissolveAmount);
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if(dissolveAmount < 1)
+    //    { 
+    //        dissolveAmount += dissolveSpeed * Time.deltaTime;
+    //        dissolveMat.SetFloat("Vector1_D7B00FB3", dissolveAmount);
+    //        Debug.Log(dissolveAmount);
+    //    }
+    //}
+    public float DissolveSpeed;
+    [HideInInspector]
+    public bool Fall = false;
+    public Material DissolveMat;
+
+
+    private float DissolveAmount = -1;
+    private bool DissolveShader = false;
+
+    private void Start()
+    {
+        DissolveMat.SetFloat("Vector1_D7B00FB3", DissolveAmount);
+    }
+
+    private void Update()
+    {
+        if (DissolveAmount < 1 && DissolveShader)
+        {
+            DissolveAmount += DissolveSpeed * Time.deltaTime;
+            DissolveMat.SetFloat("Vector1_D7B00FB3", DissolveAmount);
+            //Debug.Log(DissolveAmount);
+        }
+        if (DissolveAmount > 0.9f)
+        {
+            Fall = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            DissolveShader = true;
+        }
+    }
+}
